71d165c7d7c0c62d0c43d61e66cdeb8240bd5f0c
Cy Messmer <cy.messmer@gmail.com>
2026-04-26 17:47:40 -0400

    structs created to model DB

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-
+use time::OffsetDateTime;
 // Shared data models go here. Both frontend and backend depend on this crate.
 // Types that cross the network boundary must derive Serialize + Deserialize.
 
@@ -7,4 +7,54 @@ use serde::{Deserialize, Serialize};
 pub struct Technician {
     pub id: u32,
     pub name: String,
+    #[serde(with = "time::serde::rfc3339")]
+    pub created_at: OffsetDateTime,
+    #[serde(with = "time::serde::rfc3339")]
+    pub updated_at: OffsetDateTime
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Service {
+    pub id: u32,
+    pub service_type: HvacServiceType,
+    pub price: u32, // Stored as cents. 
+    #[serde(with = "time::serde::rfc3339")]
+    pub created_at: OffsetDateTime,
+    #[serde(with = "time::serde::rfc3339")]
+    pub updated_at: OffsetDateTime
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum HvacServiceType {
+    Maintenance,
+    Installation,
+    Repair,
+    Emergency
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Customer {
+    pub id: u32,
+    pub name: String,
+    pub email: String, //ToDo: validate email regex: '^[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$'
+    pub phone_number: String, //ToDo: format as 10 digit code
+    #[serde(with = "time::serde::rfc3339")]
+    pub created_at: OffsetDateTime,
+    #[serde(with = "time::serde::rfc3339")]
+    pub updated_at: OffsetDateTime
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Appointment {
+    pub id: u32,
+    pub technician_id: u32,
+    pub service_id: u32,
+    pub customer_id: u32,
+    // ISO8601:RFC3339 DateTime for compatibility outside Rust
+    #[serde(with = "time::serde::rfc3339")]
+    pub scheduled_time: OffsetDateTime,
+    #[serde(with = "time::serde::rfc3339")]
+    pub created_at: OffsetDateTime,
+    #[serde(with = "time::serde::rfc3339")]
+    pub updated_at: OffsetDateTime
 }
